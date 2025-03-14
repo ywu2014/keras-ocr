f2dee001d996e406244f78da00d52b4be51467c6
ywu2014 <yejunwu123@gmail.com>
2025-03-14 14:32:49 +0800

    feat: 端到端训练脚本

---
+++ train/end_to_end_training.py
@@ -0,0 +1,150 @@
+import zipfile
+import datetime
+import string
+import math
+import os
+
+import tqdm
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import sklearn.model_selection
+
+import keras_ocr
+
+## Generating synthetic data
+assert tf.test.is_gpu_available(), 'No GPU is available.'
+
+data_dir = '.'
+alphabet = string.digits + string.ascii_letters + '!?. '
+recognizer_alphabet = ''.join(sorted(set(alphabet.lower())))
+fonts = keras_ocr.data_generation.get_fonts(
+    alphabet=alphabet,
+    cache_dir=data_dir
+)
+backgrounds = keras_ocr.data_generation.get_backgrounds(cache_dir=data_dir)
+
+
+text_generator = keras_ocr.data_generation.get_text_generator(alphabet=alphabet)
+print('The first generated text is:', next(text_generator))
+
+def get_train_val_test_split(arr):
+    train, valtest = sklearn.model_selection.train_test_split(arr, train_size=0.8, random_state=42)
+    val, test = sklearn.model_selection.train_test_split(valtest, train_size=0.5, random_state=42)
+    return train, val, test
+
+background_splits = get_train_val_test_split(backgrounds)
+font_splits = get_train_val_test_split(fonts)
+
+image_generators = [
+    keras_ocr.data_generation.get_image_generator(
+        height=640,
+        width=640,
+        text_generator=text_generator,
+        font_groups={
+            alphabet: current_fonts
+        },
+        backgrounds=current_backgrounds,
+        font_size=(60, 120),
+        margin=50,
+        rotationX=(-0.05, 0.05),
+        rotationY=(-0.05, 0.05),
+        rotationZ=(-15, 15)
+    )  for current_fonts, current_backgrounds in zip(
+        font_splits,
+        background_splits
+    )
+]
+
+# See what the first validation image looks like.
+image, lines = next(image_generators[1])
+text = keras_ocr.data_generation.convert_lines_to_paragraph(lines)
+print('The first generated validation image (below) contains:', text)
+plt.imshow(image)
+
+## Build base detector and recognizer models
+detector = keras_ocr.detection.Detector(weights='clovaai_general')
+recognizer = keras_ocr.recognition.Recognizer(
+    alphabet=recognizer_alphabet,
+    weights='kurapan'
+)
+recognizer.compile()
+for layer in recognizer.backbone.layers:
+    layer.trainable = False
+
+## Train the detector
+detector_batch_size = 1
+detector_basepath = os.path.join(data_dir, f'detector_{datetime.datetime.now().isoformat()}')
+detection_train_generator, detection_val_generator, detection_test_generator = [
+    detector.get_batch_generator(
+        image_generator=image_generator,
+        batch_size=detector_batch_size
+    ) for image_generator in image_generators
+]
+detector.model.fit(
+    detection_train_generator,
+    steps_per_epoch=math.ceil(len(background_splits[0]) / detector_batch_size),
+    epochs=1000,
+    workers=0,
+    callbacks=[
+        tf.keras.callbacks.EarlyStopping(restore_best_weights=True, patience=5),
+        tf.keras.callbacks.CSVLogger(f'{detector_basepath}.csv'),
+        tf.keras.callbacks.ModelCheckpoint(filepath=f'{detector_basepath}.h5')
+    ],
+    validation_data=detection_val_generator,
+    validation_steps=math.ceil(len(background_splits[1]) / detector_batch_size),
+    batch_size=detector_batch_size
+)
+
+## Train the recognizer
+max_length = 10
+recognition_image_generators = [
+    keras_ocr.data_generation.convert_image_generator_to_recognizer_input(
+        image_generator=image_generator,
+        max_string_length=min(recognizer.training_model.input_shape[1][1], max_length),
+        target_width=recognizer.model.input_shape[2],
+        target_height=recognizer.model.input_shape[1],
+        margin=1
+    ) for image_generator in image_generators
+]
+
+# See what the first validation image for recognition training looks like.
+image, text = next(recognition_image_generators[1])
+print('This image contains:', text)
+plt.imshow(image)
+
+recognition_batch_size = 8
+recognizer_basepath = os.path.join(data_dir, f'recognizer_{datetime.datetime.now().isoformat()}')
+recognition_train_generator, recognition_val_generator, recognition_test_generator = [
+    recognizer.get_batch_generator(
+    image_generator=image_generator,
+    batch_size=recognition_batch_size,
+    lowercase=True
+    ) for image_generator in recognition_image_generators
+]
+recognizer.training_model.fit(
+    recognition_train_generator,
+    epochs=1000,
+    steps_per_epoch=math.ceil(len(background_splits[0]) / recognition_batch_size),
+    callbacks=[
+        tf.keras.callbacks.EarlyStopping(restore_best_weights=True, patience=25),
+        tf.keras.callbacks.CSVLogger(f'{recognizer_basepath}.csv', append=True),
+        tf.keras.callbacks.ModelCheckpoint(filepath=f'{recognizer_basepath}.h5')
+    ],
+    validation_data=recognition_val_generator,
+    validation_steps=math.ceil(len(background_splits[1]) / recognition_batch_size),
+    workers=0,
+    bacth_size=recognition_batch_size
+)
+
+## Use the models for inference
+pipeline = keras_ocr.pipeline.Pipeline(detector=detector, recognizer=recognizer)
+image, lines = next(image_generators[0])
+predictions = pipeline.recognize(images=[image])[0]
+drawn = keras_ocr.tools.drawBoxes(
+    image=image, boxes=predictions, boxes_format='predictions'
+)
+print(
+    'Actual:', '\n'.join([' '.join([character for _, character in line]) for line in lines]),
+    'Predicted:', [text for text, box in predictions]
+)
+plt.imshow(drawn)
